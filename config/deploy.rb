4d9053cfbf21ec5e6130ffd871db223732a4b91e
e440 <jvejar1@miuandes.cl>
2018-07-30 19:51:37 -0400

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.1"
+lock "~> 3.11.0"
 require "bundler/capistrano"
 set :application, "jgapi"
 set :repo_url, "https://github.com/jvejar1/jgapi.git"
