71035aa4fb1a520ae87dedcf5d39128203771778
esteban <jvejar1@miuandes.cl>
2021-01-08 22:12:47 -0300

    minor fixes

--- users_generator.py
+++ users_generator.py
@@ -4,26 +4,8 @@ letters.extend([chr(i) for i in range(48,57)])
 letters.extend([chr(i) for i in range(65,90)])
 
 letters.extend([chr(i) for i in range(97,122)])
-users=[ "mugarte2@miuandes.cl",
-"imgarrido@miuandes.cl",
-"cminzunza@miuandes.cl",
-"eloy.navarrete@hotmail.com",
-"oscar.rodriguez.psi@gmail.com","3592294@gmail.com",
-"bcaros@uc.cl",
-"Jfhenriquez@miuandes.cl",
-"pamela.figueroaas.@gmail.com",
-"tamarshantal@icloud.com", 
-"mjcampero@uc.cl",
-"alfuentealba@uc.cl",
-"skclaussen@miuandes.cl",
-"manuelfsc78@gmail.com",
-"pemorata@uc.cl",
-"jgvaldivieso@uc.cl", 
-"caperez14@uc.cl",
-"mlvaldivia@uc.cl",
-"ignacio.galvezr@mail.udp.cl",
-"valeria.troncoso@usach.cl",
-"ipgonzale@miuandes.cl"]
+users=[ "mfuentes15@alumnos.utalca.cl", 
+"dcerda15@alumnos.utalca.cl"]
 
 print letters
 for user in users:
