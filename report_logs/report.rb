8d3d5de3ba26a4fac68c40c08d52e01bf7fe420e
jvejar1 <jvejar1@miuandes.cl>
2021-05-26 21:57:03 -0400

    report updated

--- report_logs/report.rb
+++ report_logs/report.rb
@@ -6,5 +6,8 @@ n_corsi_answers=CsequenceAnswer.all.count
 n_wally_answers=WsituationAnswer.all.count
 n_hnf_answers=HnfAnswer.all.count
 n_users = User.all.count
-
-puts "n_evals: #{n_evals} \n n_items: #{n_items} \n n_answers: #{n_answers} \n n_open_answers: #{n_open_answers} \n n_corsi_answers:#{n_corsi_answers} \n n_wally_answers: #{n_wally_answers} \n n_hnf_answers: #{n_hnf_answers} \n n_users: #{n_users}"
+n_schools = School.all.count
+n_courses = Course.all.count
+n_student_course = StudentCourse.all.count
+n_students =Student.all.count
+puts "n_evals: #{n_evals} \n n_items: #{n_items} \n n_answers: #{n_answers} \n n_open_answers: #{n_open_answers} \n n_corsi_answers:#{n_corsi_answers} \n n_wally_answers: #{n_wally_answers} \n n_hnf_answers: #{n_hnf_answers} \n n_users: #{n_users} \n n_schools: #{n_schools} \n n_courses: #{n_courses} \n n_student_courses: #{n_student_course} \n n_students: #{n_students} \n END"
