4a50b45ec0b1cf12f6ab6fc48891c5f8de61743a
jvejar1 <jvejar1@miuandes.cl>
2021-06-10 19:59:46 -0400

    selecting the choices considering the order as default scope

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -3,4 +3,5 @@ class Choice < ApplicationRecord
   belongs_to :parent, class_name: 'Choice', foreign_key: :parent_choice_id
   has_many :children, class_name: 'Choice', foreign_key: :parent_choice_id
   belongs_to :picture
+  default_scope {order(order: :asc)}
 end
